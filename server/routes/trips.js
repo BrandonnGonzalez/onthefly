601be41e33082b27459eac3ee231c3a76923cfa4
Brandon <brandongonzalezswe@gmail.com>
2026-04-02 15:14:10 -0400

    // defining the route handlers for the /trips endpoint using the corresponding controller functions from the TripsController

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -3,7 +3,12 @@ import TripsController from '../controllers/trips.js'
 
 const router = express.Router();
 
-
+// defining the route handlers for the /trips endpoint using the corresponding controller functions from the TripsController
+router.get('/', TripsController.getTrips);
+router.get('/:id', TripsController.getTrip);
+router.post('/', TripsController.createTrip);
+router.delete('/:id', TripsController.deleteTrip);
+router.patch('/:id', TripsController.updateTrip);
 
 
 export default router
