baf3c9290c0c97df57d3f87183d992d7b0249ef5
Brandon <brandongonzalezswe@gmail.com>
2026-04-02 15:05:25 -0400

    Changed quotation marks to backticks since the sql queries were multi-line

--- server/config/controllers/trips.js
+++ server/config/controllers/trips.js
@@ -5,9 +5,9 @@ const createTrip = async (req, res) => {
   try {
     const { title, description, img_url, num_days, start_date, end_date, total_cost } = req.body
     const results = await pool.query(
-      'INSERT INTO trips (title, description, img_url, num_days, start_date, end_date, total_cost)
+      `INSERT INTO trips (title, description, img_url, num_days, start_date, end_date, total_cost)
       VALUES($1, $2, $3, $4, $5, $6, $7)
-      RETURNING *',
+      RETURNING *`,
       [title, description, img_url, num_days, start_date, end_date, total_cost]
     )
 
@@ -47,9 +47,9 @@ const updateTrip = async (req, res) => {
 
     try {
         const results = await pool.query(
-            'UPDATE trips
-            SET title = $1, description = $2, img_url = $3, num_days = $4, start_date = $5, end_date = $6, total_cost= $
-            WHERE id = $8',
+            `UPDATE trips
+            SET title = $1, description = $2, img_url = $3, num_days = $4, start_date = $5, end_date = $6, total_cost = $7
+            WHERE id = $8`,
             [title, description, img_url, num_days, start_date, end_date, total_cost, id]
         );
         res.status(200).json(results.rows[0]);
